681257504cd4f77bf142ab6c4441323db4391cf1
DIEMVO <43290038+DIEMVO@users.noreply.github.com>
2020-06-25 16:42:52 +0700

    up
    up

---
+++ Website_BanDienThoai_Version1.2/Website_BanDienThoai_Version1/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Website_BanDienThoai_Version1.Data;
+using Website_BanDienThoai_Version1.Models;
+
+namespace Website_BanDienThoai_Version1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var users = _db.Users.FromSql("EXECUTE DBO.Select_All_User");
+         
+            return View(users);
+        }
+        //Get Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Database.ExecuteSqlCommand("EXECUTE DBO.Insert_Users {0},{1},{2},{3},{4},{5},{6}",
+                   user.UserName, user.Password, user.Name, user.Email, user.Phone, user.DateOfBith,user.Gender);
+
+                await _db.SaveChangesAsync();
+          ;
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        // Get Edit Action Method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var user = await _db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //POST Edit action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Users user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+               
+               _db.Database.ExecuteSqlCommand("EXECUTE DBO.Update_User {0},{1},{2},{3},{4},{5},{6},{7}", id,
+                   user.UserName, user.Password, user.Name, user.Email, user.Phone, user.DateOfBith, user.Gender);
+                _db.Entry(user).Reload();
+                await _db.SaveChangesAsync();
+                //_db.Update(user);
+                //await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        // Get Delete Action Method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var user = await _db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        //POST Delete action Method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+            _db.Database.ExecuteSqlCommand("EXECUTE DBO.Delete_User {0}", id);
+            await _db.SaveChangesAsync();
+            //_db.Users.Remove(user);
+            //await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Website_BanDienThoai_Version1.2/Website_BanDienThoai_Version1/Models/ViewModel/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website_BanDienThoai_Version1.Models.ViewModel
+{
+    public class ProductViewModel
+    {
+        public Products Products { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<SpecialTag> SpecialTag { get; set; }
+    }
+}
