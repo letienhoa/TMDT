2ef44279ba5020502b0ae357a93fdf46dba170a3
giahantran <43287445+giahantran@users.noreply.github.com>
2020-06-25 19:34:16 +0700

    update giao dien Login (ver 1.3)

---
+++ Website_BanDienThoai_Version1.3/Website_BanDienThoai_Version1/Migrations/20191108234230_addCategory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Website_BanDienThoai_Version1.Migrations
+{
+    public partial class addCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
---
+++ Website_BanDienThoai_Version1.3/Website_BanDienThoai_Version1/Migrations/20191113132915_DatabaseNew.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Website_BanDienThoai_Version1.Migrations
+{
+    public partial class DatabaseNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Bill_Details_BillId1",
+                table: "Bill_Details",
+                column: "BillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bill_Details_BillId1",
+                table: "Bill_Details");
+        }
+    }
+}
