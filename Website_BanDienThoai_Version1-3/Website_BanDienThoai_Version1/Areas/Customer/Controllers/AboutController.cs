880431bdddfafceada375530d4618918d0f50031
Hoale <nhoxlexit@yahoo.com.vn>
2020-06-27 00:17:54 +0700

    moi

---
+++ Website_BanDienThoai_Version1-3/Website_BanDienThoai_Version1/Areas/Customer/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website_BanDienThoai_Version1.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website_BanDienThoai_Version1-3/Website_BanDienThoai_Version1/Areas/Customer/Controllers/PhoneController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Website_BanDienThoai_Version1.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Website_BanDienThoai_Version1.Extentions;
+namespace Website_BanDienThoai_Version1.Controllers
+{
+    [Area("Customer")]
+    public class PhoneController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PhoneController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.Products.Include(m => m.Category).Include(m => m.SpecialTag).ToListAsync();
+            return View(productList);
+      
+        }
+    }
+}
